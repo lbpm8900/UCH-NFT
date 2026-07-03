ad71aacc03bc3dbc9bc1dcafe2ee3db63119795b
lbpm8900 <85262797+lbpm8900@users.noreply.github.com>
2026-07-02 22:39:35 -0700

    Delete uch_nft_final_github_site (2) directory

--- uch_nft_final_github_site (2)/README.md
+++
@@ -1,5 +0,0 @@
-# UCH NFT
-
-Official website for the UCH NFT Historic United States Stamp Collection.
-
-OpenSea: https://opensea.io/UCH-NFT
--- uch_nft_final_github_site (2)/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('a[href*="opensea.io"]').forEach((link) => {
-  link.addEventListener('click', () => {
-    console.log('Opening official UCH NFT OpenSea collection');
-  });
-});
